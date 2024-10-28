9e268dbcd1a3e4176a30f8724fad526d1be8a8d0
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-10-28 21:02:11 +0500

    runner test

--- fake_math.py
+++ fake_math.py
@@ -1,5 +1,29 @@
-def delenie(a, b):
-    if (b!=0):
-        return a/b
-    else:
-        return 'Ошибка'
+import urban
+import unittest
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        a = urban.Runner('turtle')
+        for i in range(10):
+            a.walk()
+        self.assertEqual(a.distance, 50)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run(self):
+        a = urban.Runner('frog')
+        for i in range(10):
+            a.run()
+        self.assertEqual(a.distance, 100)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_challenge(self):
+        a = urban.Runner('pomidurov')
+        b = urban.Runner('oguchok')
+        for i in range(10):
+            a.run()
+            b.walk()
+        self.assertNotEqual(a.distance, b.distance)
